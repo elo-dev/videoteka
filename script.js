e802643fcb498205f11d788623ab4e4d56265f50
Dima Frolov <dimasiys25@gmail.com>
2021-02-01 02:42:29 +0300

    Возможность добавлять карточки

--- script.js
+++ script.js
@@ -25,9 +25,11 @@ modal.addEventListener("click", handleModalClick);
 
 const btnAddVideo = document.querySelector('.btn-success');
 const blockAddVideo = document.querySelector('.addVideo');
+const blockTopVideo = document.querySelector('.topVideo');
 
 btnAddVideo.addEventListener('click', function(){
-  blockAddVideo.classList.remove('addVideo');
+  blockAddVideo.hidden = false;
+  blockTopVideo.hidden = true;
 });
 
 const addVideo = document.querySelector('.btn-success-form');
@@ -51,6 +53,28 @@ addVideo.addEventListener('click', function(event){
     errFile.textContent = 'Файл для загрузки не выбран';
     error.appendChild(errFile);
   }
+
+  var cardWrapper = document.querySelector('.cardVideo-row');
+
+  console.log(cardWrapper);
+  
+  const videoCard = `
+  <div class="col mb-4">
+    <div class="card">
+      <img src="${url}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${nameVideo}</h5>
+        <div class="wrapper__footer-card">
+          <span class="fa fa-star rating-star"></span>
+          <span class="card-rate">${checkbox}</span>
+          <span class="card-genere">${genere}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+
+  cardWrapper.insertAdjacentHTML('beforeend', videoCard);
 })
 
 var rateCheckbox = document.querySelectorAll('[type="radio"]');
@@ -80,9 +104,10 @@ var checkbox;
   });
 
   var blockPreview = document.querySelector('.wrapper__form__preview');
+  var url;
 
-  document.getElementById("imageVideo").addEventListener('change',function(e){
-    var url = window.URL.createObjectURL(e.target.files[0]);
+  document.getElementById("imageVideo").addEventListener('change',function (e){
+    url = window.URL.createObjectURL(e.target.files[0]);
     var img = document.createElement('img');
     img.id = 'form__imgPreview';
     blockPreview.insertAdjacentElement('beforeend', img);
@@ -95,28 +120,43 @@ var checkbox;
 
 document.querySelector('.btn-preview').addEventListener('click', function(e){
   e.preventDefault();
-  // Добавление названия в блок preview
   var nameVideo = document.querySelector('.nameVideo').value;
-  var pNameVideo = document.createElement('p');
-      pNameVideo.id = 'preview__nameVideo';
-      blockPreview.insertAdjacentElement('beforeend', pNameVideo);
-      pNameVideo.insertAdjacentText('beforeend', nameVideo);
-  // Добавление рейтинга в блок preview
-  var rateVideo = document.createElement('span');
-  var spanRateVideo = document.createElement('span');
-  var spanRateVideoStar = document.createElement('span');
-      spanRateVideoStar.classList.add("fa","fa-star","rating-star");
-      rateVideo.id = 'preview__rateVideo';
-      spanRateVideo.id = 'preview__wrapper__rateVideo';
-      spanRateVideo.insertAdjacentElement('beforeend', spanRateVideoStar);
-      blockPreview.insertAdjacentElement('beforeend', spanRateVideo);
-      spanRateVideo.insertAdjacentElement('beforeend', rateVideo);
-      rateVideo.insertAdjacentText('beforeend', checkbox);
-  // Добавление жанра в блок preview
-  var pGenereVideo = document.createElement('span');
-      pGenereVideo.id = 'preview__genereVideo';
-      spanRateVideo.insertAdjacentElement('beforeend', pGenereVideo);
-      pGenereVideo.insertAdjacentText('beforeend', genere);
-});
+  var error = document.querySelector('.error');
 
-  
+  // Проверка на пустоту полей
+  if(nameVideo !== '' && typeof checkbox !== 'undefined' && typeof genere !== 'undefined'){
+  // Добавление названия в блок preview
+    var pNameVideo = document.createElement('p');
+        pNameVideo.id = 'preview__nameVideo';
+        blockPreview.insertAdjacentElement('beforeend', pNameVideo);
+        pNameVideo.insertAdjacentText('beforeend', nameVideo);
+  // Добавление рейтинга в блок preview
+    var rateVideo = document.createElement('span');
+    var spanRateVideo = document.createElement('span');
+    var spanRateVideoStar = document.createElement('span');
+        spanRateVideoStar.classList.add("fa","fa-star","rating-star");
+        rateVideo.id = 'preview__rateVideo';
+        spanRateVideo.id = 'preview__wrapper__rateVideo';
+        spanRateVideo.insertAdjacentElement('beforeend', spanRateVideoStar);
+        blockPreview.insertAdjacentElement('beforeend', spanRateVideo);
+        spanRateVideo.insertAdjacentElement('beforeend', rateVideo);
+        rateVideo.insertAdjacentText('beforeend', checkbox);
+  // Добавление жанра в блок preview
+    var pGenereVideo = document.createElement('span');
+        pGenereVideo.id = 'preview__genereVideo';
+        spanRateVideo.insertAdjacentElement('beforeend', pGenereVideo);
+        pGenereVideo.insertAdjacentText('beforeend', genere);
+  } else if(nameVideo !== '' && typeof checkbox === 'undefined'){
+    var errRatePreview = document.createElement('p');
+        errRatePreview.textContent = 'Вы не указали рейтинг';
+        error.appendChild(errRatePreview);
+  } else if(nameVideo === '' && typeof checkbox !== 'undefined'){
+    var errNamePreview = document.createElement('p');
+        errNamePreview.textContent = 'Вы не указали название';
+        error.appendChild(errNamePreview);
+  } else{
+    var errPreview = document.createElement('p');
+        errPreview.textContent = 'Вы не указали поле название, рейтинг и жанр';
+        error.appendChild(errPreview);
+  }
+});
